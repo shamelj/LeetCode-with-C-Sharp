e6ae01db34b74b95ec34a15826bd01c2bf4d35f8
shamelj <70355802+shamelj@users.noreply.github.com>
2022-07-19 13:13:50 +0300

    26. Remove Duplicates from Sorted Array

---
+++ LeetCode/_26_RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------------
+// Runtime: 164 ms
+// Memory Usage: 46.1 MB
+// Link: https://leetcode.com/submissions/detail/751018763/
+//-----------------------------------------------------------------------------
+namespace LeetCode;
+public class _26_RemoveDuplicatesFromSortedArray
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        int l = 0, r = 1;
+        while(r< nums.Length)
+        {
+            if (nums[r] != nums[l])
+                nums[++l] = nums[r];
+            r++;
+        }
+        return l + 1;
+    }
+}
